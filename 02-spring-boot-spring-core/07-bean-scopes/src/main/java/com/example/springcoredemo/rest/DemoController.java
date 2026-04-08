60d8c5f7be0387b783597ef80697a16037713563
pis82009wcr <pis82009wcr@gmail.com>
2026-04-08 16:11:31 -0500

    add 06-lazy-initialization 07-bean-scope 08-bean-lifecycle-method 09-java-config-bean

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -0,0 +1,34 @@
+package com.example.springcoredemo.rest;
+
+import com.example.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define a private field for the dependency
+    private Coach myCoach;
+    private Coach anotherCoach;
+
+
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") Coach coach,
+            @Qualifier("cricketCoach") Coach theanotherCoach) {
+        myCoach = coach;
+        anotherCoach = theanotherCoach;
+        System.out.println("In constructor : " + getClass().getSimpleName());
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == antherCoach, " + (myCoach == anotherCoach);
+    }
+}
